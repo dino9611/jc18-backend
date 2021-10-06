296777578e9a062615d0e3057d2a8a2938014862
dino9611 <aldinorahman36@gmail.com>
2021-10-06 12:25:50 +0700

    day 4 sesi1

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const PORT = 5000;
 const { tampilakanHtml } = require("./helpers");
 
@@ -20,26 +21,48 @@ let users = [
 let products = [
   {
     id: 1,
-    name: "tes1",
-    keterangan: "abce",
+    name: "papan kayu",
+    price: 20000,
   },
   {
     id: 2,
-    name: "tes2",
-    keterangan: "abce",
+    name: "kursi kayu",
+    price: 200000,
+  },
+  {
+    id: 3,
+    name: "lemari kece",
+    price: 50000,
+  },
+  {
+    id: 4,
+    name: "jendela bagus",
+    price: 100000,
   },
 ];
-let id = 2;
+
+let id = 4;
+
 const loggingFunc = (req, res, next) => {
   console.log(req.method, req.url, new Date().toString());
   // nambah property dino di object req
-  req.dino = "namaku";
+  // req.dino = "namaku";
+
   next();
 };
+
+//? cara manual
+// const cors = (req, res, next) => {
+//   res.set("Access-Control-Allow-Origin", "*");
+//   res.set("Access-Control-Allow-Methods", "GET,OPTIONS,POST,PUT,DELETE,PATCH");
+//   res.set("Access-Control-Allow-Headers", "content-type");
+//   next();
+// };
 
 // middleware global start
 app.use(express.json());
 app.use(loggingFunc);
+app.use(cors()); // klo corsnya "cors()" artinya allow semua ip
 // ini midlleware untuk nampung data body untuk method post,put,patch
 
 // middleware end
@@ -50,15 +73,44 @@ app.use(loggingFunc);
 //   return res.status(200).send(tampilanWelcome);
 // });
 
+// method PUT,PATCH,POST punya req.query,req.params,req.body
+// method GET req.query,req.params
+
 app.get("/", async (req, res) => {
-  console.log(req.dino, "dari function sebelumnya");
+  // console.log(req.dino, "dari function/middleware sebelumnya");
   let tampilanWelcome = await tampilakanHtml("./index.html");
   return res.status(200).send(tampilanWelcome);
 });
 
+app.get("/products/:id", (req, res) => {
+  const { id } = req.params;
+  let indexProd = products.findIndex((val) => val.id == id);
+  return res.status(200).send(products[indexProd]);
+});
+
 app.get("/products", (req, res) => {
-  console.log(req.dino, "dari function sebelumnya");
-  return res.status(200).send(products);
+  // console.log(req.dino, "dari function sebelumnya");
+  console.log("63 req.query products", req.query);
+  const { namaProd, hargaMin, hargaMax } = req.query;
+  let newFilterProd = products;
+
+  if (namaProd) {
+    // jika nameProd ada isinya filter
+    newFilterProd = newFilterProd.filter((val) =>
+      val.name.toLowerCase().includes(namaProd.toLowerCase())
+    );
+  }
+  if (hargaMin) {
+    // jika hargamin ada isinya filter
+    newFilterProd = newFilterProd.filter((val) => val.price >= hargaMin);
+  }
+  if (hargaMax) {
+    // jika hargamax ada isinya filter
+    newFilterProd = newFilterProd.filter((val) => val.price <= hargaMax);
+  }
+  // jika semua querynya false pasti tidak difilter artinya newfilterprod tidak ada perubahan
+  // newFilterProd adalah array of object
+  return res.status(200).send(newFilterProd);
 });
 
 app.get("/users", (req, res) => {
@@ -73,6 +125,31 @@ app.post("/products", (req, res) => {
   // push data to database
   products.push(data);
   return res.status(200).send(products);
+});
+// edit
+app.patch("/products/:id", (req, res) => {
+  const { id } = req.params;
+  // cari index
+  let indexEdit = products.findIndex((val) => val.id == id);
+
+  if (indexEdit >= 0) {
+    // edit data
+    const { name, price } = req.body;
+    if (name) {
+      // jika name ada
+      products[indexEdit].name = name;
+    }
+    if (price) {
+      // jika price ada
+      products[indexEdit].price = price;
+    }
+    return res.status(200).send(products);
+  } else {
+    let obj = {
+      message: "tidak ada id",
+    };
+    return res.status(400).send(obj);
+  }
 });
 
 app.delete("/products/:id", (req, res) => {

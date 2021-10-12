7c4ebe7a8569d88f8727138f7e0e19b3da8e8e05
dino9611 <aldinorahman36@gmail.com>
2021-10-12 15:56:26 +0700

    day8 sesi2

--- index.js
+++ index.js
@@ -71,29 +71,59 @@ app.get("/products/:id", (req, res) => {
   return res.status(200).send(products[indexProd]);
 });
 
-app.get("/products", (req, res) => {
-  // console.log(req.dino, "dari function sebelumnya");
-  // console.log("63 req.query products", req.query);
-  const { namaProd, hargaMin, hargaMax } = req.query;
-  let newFilterProd = products;
-
+app.get("/products", async (req, res) => {
+  // pages=1 dan limit=2 default value jika req.query tidak ada
+  const { namaProd, hargaMin, hargaMax, pages = 1, limit = 2 } = req.query;
+  // console.log(req.query);
+  // console.log(limit);
+  // offset mulai dari row berapa di sql jika offset 0 maka row 1 sebanyak "limit" akan diambil ,
+  // jika offset 10 maka row 11 sebanyak "limit" akan diambil
+  let offset = (pages - 1) * limit;
+  let querySql = ` where true`;
+  const connDb = connection.promise();
   if (namaProd) {
     // jika nameProd ada isinya filter
-    newFilterProd = newFilterProd.filter((val) =>
-      val.name.toLowerCase().includes(namaProd.toLowerCase())
-    );
+    querySql += ` and name like ${connection.escape("%" + namaProd + "%")}`;
   }
   if (hargaMin) {
     // jika hargamin ada isinya filter
-    newFilterProd = newFilterProd.filter((val) => val.price >= hargaMin);
+    querySql += ` and price >= ${connection.escape(parseInt(hargaMin))}`;
   }
   if (hargaMax) {
     // jika hargamax ada isinya filter
-    newFilterProd = newFilterProd.filter((val) => val.price <= hargaMax);
+    querySql += ` and price <= ${connection.escape(parseInt(hargaMax))}`;
   }
+  try {
+    let sql = `select * from products ${querySql} limit ?,? `;
+    console.log(sql);
+    const [dataProduct] = await connDb.query(sql, [offset, parseInt(limit)]);
+    sql = `select count(*) as total_data from products ${querySql}`;
+    const [totalData] = await connDb.query(sql);
+    // hasil dari sql akan selalu menghasilkan array of object
+    // hasilnya harus menyertakan total dan, pages berapa
+    return res.status(200).send({
+      pages: parseInt(pages),
+      total: totalData[0].total_data,
+      data: dataProduct,
+    });
+  } catch (error) {
+    console.log("error :", error);
+    return res.status(500).send({ message: error.message });
+  }
+
+  // try {
+  //   let sql = `select * from products`;
+  //   const [dataProduct] = await connDb.query(sql);
+  //   return res.status(200).send({
+  //     data: dataProduct,
+  //   });
+  // } catch (error) {
+  //   console.log("error :", error);
+  //   return res.status(500).send({ message: error.message });
+  // }
+
   // jika semua querynya false pasti tidak difilter artinya newfilterprod tidak ada perubahan
   // newFilterProd adalah array of object
-  return res.status(200).send(newFilterProd);
 });
 
 app.get("/login", (req, res) => {
@@ -300,38 +330,60 @@ app.get("/users", async (req, res) => {
   // });
 });
 
-app.post("/products", (req, res) => {
+app.post("/products", async (req, res) => {
   console.log(req.body);
-  let data = req.body;
-  data.id = ++id;
-  // push data to database
-  products.push(data);
-  return res.status(200).send(products);
+  const { name, price } = req.body;
+  // kalo mau buat proteksi
+  if (!name || !price) {
+    return res.status(400).send({ message: "kurang name or price" });
+  }
+  //? cara mysql2 promise
+  // add data product to table products
+  let sql = `insert into products set ?`;
+  try {
+    let dataInsert = {
+      name: name,
+      price,
+    };
+    const [results] = await connection.promise().query(sql, dataInsert);
+    console.log(results); // resultsnya insert itu object, property insertId lumayan penting
+    // get semua data products
+    sql = `select * from products `;
+    const [userData] = await connection.promise().query(sql);
+    return res.status(200).send(userData);
+  } catch (err) {
+    console.log("error :", err);
+    return res.status(500).send({ message: err.message });
+  }
 });
 // edit
-app.patch("/products/:id", (req, res) => {
+app.patch("/products/:id", async (req, res) => {
   const { id } = req.params;
-  // cari index
-  let indexEdit = products.findIndex((val) => val.id == id);
+  const connDb = connection.promise();
 
-  if (indexEdit >= 0) {
-    // edit data
-    const { name, price } = req.body;
-    if (name) {
-      // jika name ada
-      products[indexEdit].name = name;
+  try {
+    // cek product by id
+    let sql = `select id from products where id = ?`;
+    let [dataProd] = await connDb.query(sql, [id]);
+    if (!dataProd.length) {
+      // kalo kosong atau data tidak ada
+      return res.status(500).send({ message: "id tidak ditemukan" });
     }
-    if (price) {
-      // jika price ada
-      products[indexEdit].price = price;
-    }
-    return res.status(200).send(products);
-  } else {
-    let obj = {
-      message: "tidak ada id",
-    };
-    return res.status(400).send(obj);
+    let dataUpdate = req.body;
+
+    console.log("DATA UPDATE :", dataUpdate);
+    // update product
+    sql = `update products set ? where id = ?`;
+    await connDb.query(sql, [dataUpdate, id]);
+    sql = `select * from products `;
+    const [userData] = await connDb.query(sql);
+    return res.status(200).send(userData);
+  } catch (err) {
+    console.log("error :", err);
+    return res.status(500).send({ message: err.message });
   }
+
+  // return res.status(400).send(obj);
 });
 
 app.delete("/products/:id", (req, res) => {
@@ -348,6 +400,44 @@ app.delete("/products/:id", (req, res) => {
       message: "tidak ada id",
     };
     return res.status(400).send(obj);
+  }
+});
+
+app.post("/auth/register", async (req, res) => {
+  const { username, password } = req.body;
+  // initialize for using promisi in mysqljs
+  const connDb = connection.promise();
+  try {
+    // TODO:
+    // cek username ada atau enggak
+    // kalo ada return status 500 dan kasih message username telah ada
+    // kalo tidak ada add data to user table
+
+    // get lagi datanya kayak login
+
+    //? cek username ada atau enggak
+    let sql = `select * from user where username = ?`;
+    let [dataUser] = await connDb.query(sql, [username]);
+    if (dataUser.length) {
+      //? kalo ada return status 500 dan kasih message username telah ada
+      throw { message: "username sudah ada" };
+    }
+    //? kalo tidak ada add data to user table
+    sql = `insert into user set ?`;
+    let dataInsert = {
+      username,
+      password,
+    };
+    let [result] = await connDb.query(sql, [dataInsert]);
+    console.log(result.insertId);
+    //? get lagi datanya kayak login
+    sql = `select * from user where id = ?`;
+    let [dataUserRes] = await connDb.query(sql, [result.insertId]);
+    // send response
+    return res.status(200).send(dataUserRes);
+  } catch (error) {
+    console.log("error :", error);
+    return res.status(500).send({ message: error.message });
   }
 });
 
